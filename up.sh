766a36fc16ea3342b4619fd9da6de445c692df38
Netpicker <156186606+netpicker@users.noreply.github.com>
2025-02-12 11:09:32 +0100

    Update up.sh, change ownership of already allocated volumes

--- up.sh
+++ up.sh
@@ -1,2 +1,3 @@
 docker compose pull
+for volume in $(docker volume ls -q | egrep 'netpicker_(policy-data|secret)'); do docker run -u 0 --rm -v $volume:/volume busybox chown -R 911:911 /volume; done
 docker compose up -d

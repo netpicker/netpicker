a255850e5c7944d4e29bf4c98d451a001e9f6214
Otto Coster <48188762+ottocoster@users.noreply.github.com>
2025-05-22 10:06:50 +0200

    Update up.sh

--- up.sh
+++ up.sh
@@ -1,3 +1,4 @@
+docker compose down
 docker compose pull
 for volume in $(docker volume ls -q | egrep 'netpicker_(policy-data|secret|git)'); do docker run -u 0 --rm -v $volume:/volume netpicker/db chown -R 911:911 /volume; done
 docker compose up -d

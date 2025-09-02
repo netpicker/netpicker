5fb9ae6c311608ad92e8dd5e588468cee666c1ad
Netpicker <156186606+netpicker@users.noreply.github.com>
2025-09-02 18:17:04 +0200

    [ab]use a neutral container to do the chowns
    netpicker/db has a posgres entrpoint that does stuff which gets tricket into warnings that are not even relevant

--- up.sh
+++ up.sh
@@ -1,4 +1,4 @@
 docker compose down
 docker compose pull
-for volume in $(docker volume ls -q | egrep 'netpicker_(policy-data|secret|git)'); do docker run -u 0 --rm -v $volume:/volume netpicker/db chown -R 911:911 /volume; done
+for volume in $(docker volume ls -q | egrep 'netpicker_(policy-data|secret|git)'); do docker run -u 0 --rm -v $volume:/volume swaggerapi/swagger-ui chown -R 911:911 /volume; done
 docker compose up -d

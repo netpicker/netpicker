a109f115ebf04dc624b49535bc13ff9751e01352
Otto Coster <48188762+ottocoster@users.noreply.github.com>
2025-09-04 10:18:22 +0200

    Move volume migration

--- up.sh
+++ up.sh
@@ -1,4 +1,3 @@
 docker compose down
 docker compose pull
-for volume in $(docker volume ls -q | egrep 'netpicker_(policy-data|secret|git)'); do docker run -u 0 --rm -v $volume:/volume swaggerapi/swagger-ui chown -R 911:911 /volume; done
 docker compose up -d

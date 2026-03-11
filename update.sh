57bb3edd1aa1a6d3760b1038e0b8bde00ff6cabf
Otto Coster <48188762+ottocoster@users.noreply.github.com>
2026-03-11 10:43:43 +0100

    Copy docker compose instead of moving

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 docker compose down
-mv docker-compose.yml docker-compose.yml.bak.$(date +%s)
+cp docker-compose.yml docker-compose.yml.bak.$(date +%s)
 git pull
 docker compose pull
 docker compose up -d
